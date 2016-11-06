d642e4ed1ebf8345897eafcb32e5d9e52fba0103
Thiago Saraiva <thsaraiva@gmail.com>
2016-11-06 23:58:24 +0000

    add City class

---
+++ app/src/main/java/com/example/android/sunshine/app/Model/City.java
@@ -0,0 +1,11 @@
+package com.example.android.sunshine.app.Model;
+
+/**
+ * Created by thsaraiva on 06/11/2016.
+ */
+
+public class City {
+
+    public int id;//city code;
+    public String name;//city Name
+}
